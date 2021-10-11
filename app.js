525eadbce3b1bd6eaac995c49a4dd548dc009234
root <root@Bens-Desktop.localdomain>
2021-10-11 11:33:12 -0700

    add index.html app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// #1
+//HTTP, Hyper Text Transfer Prototcal, is a set of rules that set standards for how users get data to or from a server
+
+//#2
+//A URL, Uniform Resource Locator, is the address for a particular internet website
+
+//3
+//DNS, Domain Name System, is a system that takes URLs and makes them human readable. It then converts them into IP addresses. 
+
+//4
+//A query string is when you place key value pairs into the URL
+
+//5
+//Two HTTp verbs are GET and POST. GET retreives some information from the server, while POST sends data tot he sever and can make changes to that server
+
+//6
+//An HTTP reqquest is a request from a user to a server which must follow the HTTP protocol 
+
+//7
+//An HTTP response is a response from a server to a client which follows the HTTP protocol 
+
+//8
+//An HTTP header provides more information about the request or response. User-agent, Accept, Host are examples of request headers. 
+//A response header example is Content-type, Set-Cookie, and Cache-Control. 
+
